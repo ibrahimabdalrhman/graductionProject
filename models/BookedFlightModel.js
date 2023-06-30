2707d4aeece11be08279f0a4dcb6a5de9e7b08f6
ibrahim abdalrhman <59165733+ibrahimabdalrhman@users.noreply.github.com>
2023-06-30 19:25:17 +0300

    update BookedfligthModel

--- models/BookedFlightModel.js
+++ models/BookedFlightModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const BookedFlightSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.ObjectId,
-    ref: 'user',
+    ref: 'User',
     required: true
   },
   totalPrice: {
