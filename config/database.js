80f2a1c8b98e31ef25659c71288933e714a9a93f
ibrahim abdalrhman <59165733+ibrahimabdalrhman@users.noreply.github.com>
2023-07-02 15:24:15 +0300

    update images

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const DB = () => {
   mongoose
     .connect(process.env.MONGO_URI)
-    .then(() => console.log("DB Connected...")).catch(err=>console.log(err));
+    .then(() => console.log("DB Connected..."))
+    .catch(err => console.log(err));
 };
 
 module.exports = DB;
