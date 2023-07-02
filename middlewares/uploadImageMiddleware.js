569353f27bf547f4e0b35ab16c042e5374055f97
ibrahim abdalrhman <59165733+ibrahimabdalrhman@users.noreply.github.com>
2023-07-02 21:00:06 +0300

    update images

--- middlewares/uploadImageMiddleware.js
+++ middlewares/uploadImageMiddleware.js
@@ -5,11 +5,11 @@ const multerOptions = () => {
   const multerStorage = multer.memoryStorage();
 
   const multerFilter = function (req, file, cb) {
-    if (file.mimetype.startsWith("image")) {
-      cb(null, true);
-    } else {
-      cb(new ApiError("only images allowed", 400), false);
-    }
+    // if (file.mimetype.startsWith("image")) {
+    //   cb(null, true);
+    // } else {
+    //   cb(new ApiError("only images allowed", 400), false);
+    // }
   };
 
   const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
